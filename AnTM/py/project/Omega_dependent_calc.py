9d39e4c4ef4210f5a6795ed5a94b32b7b7fd65d0
redispersion <diter.koh@gmail.com>
2024-04-06 03:55:34 +0700

    NewBreath

--- AnTM/py/project/Omega_dependent_calc.py
+++
@@ -1,7 +0,0 @@
-omega_x = [i for i in range(500 , 4001, 1)]
-for i in range(0, len(omega_x)):
-    omega_x[i] = omega_x[i]/500
-U = [0]*len(omega_x)
-
-for i in range(0, len(omega_x)):
-    omega = omega_x[i]
